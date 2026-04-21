7d8dc9608edb5bbc66bb9c7e37375f9c2adc6e24
Matías Israelson <57065102+israpps@users.noreply.github.com>
2026-04-21 15:36:06 -0300

    tweak compile.bat

--- compile.bat
+++ compile.bat
@@ -1,3 +1,20 @@
-call "%ProgramFiles%\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+@echo off
+
+for %%a in (%*) do (
+  if "%%a"=="--setvar" (
+    if exist "%ProgramFiles%\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat" (
+      call "%ProgramFiles%\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+    ) else if exist "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" (
+      call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+    ) else (
+      echo Visual Studio 2022 not found.
+    )
+  )
+  if "%%a"=="--clean" (
+    msbuild "PCSX2_qt.sln" /m /v:m /p:Configuration=release /t:Clean
+  )
+)
+
+
 msbuild "PCSX2_qt.sln" /m /v:m /p:Configuration=release
 pause
