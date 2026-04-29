95a4df48f2c2ba5718686ec39bc457e70a6ee092
Matías Israelson <57065102+israpps@users.noreply.github.com>
2026-04-29 01:46:29 -0300

    fix ACFPGA addr MMIO

--- pcsx2/IopMem.cpp
+++ pcsx2/IopMem.cpp
@@ -366,10 +366,27 @@ void iopMemWrite16(u32 mem, u16 value)
 		ACRAM::Write16(mem, value);
 	} else if (t == ACSRAM_RANGE) {
 		ACSRAM::Write16(mem, value);
-	} else if ((t & 0xFF00) == 0x1241) {
+	} else if (t == 0x1241) {
 		switch (mem) {
 		case ACJV_CTR_START: Console.Warning("ACJV::START"); break;
 		case ACJV_CTR_STOP: Console.Warning("ACJV::STOP"); break;
+		case 0x1241510C:  Console.Warning("ACCORE::INTR  DISABLE_ACATA_INTR"); break;
+		case 0x1241511C:  Console.Warning("ACCORE::INTR  DISABLE_ACUART_INTR"); break;
+		// ACFPGA UPLOAD MMIO ///TODOx6: move this handling to ACFPGA.cpp
+		case 0x12416012:
+		case 0x12416014:
+		case 0x12416018:
+		case 0x12416016:
+		case 0x1241601A:
+			break;
+		// unknown addresses set to 0 on ACCORE. most likely stopping other stuff
+		//case 0x12416032: break;
+		//case 0x12416032: break;
+		//case 0x12416036: break;
+		//case 0x1241603A: break;
+		//case 0x12417000: break;
+		//case 0x1241601E: break;
+
 		default: Console.Error("%-16s %08X = %04X", "ACUNK::write16", mem, value); break;
 		}
 	} else if ((t & 0xFF00) == 0x1300) {
